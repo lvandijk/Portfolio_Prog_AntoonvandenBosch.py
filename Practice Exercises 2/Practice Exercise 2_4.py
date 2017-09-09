de97e0cf37a6bd878598e7e448362a7140ab64f9
Antoon <antoon.vandenbosch@student.hu.nl>
2017-09-09 16:30:10 +0200

    test voor git, extra print die "gittest" neerzet als test voor git

---
+++ Practice Exercises 2/Practice Exercise 2_4.py
@@ -0,0 +1,9 @@
+# Practice Exercise 2_4 (Input/Output)
+# Schrijf een programma dat de gebruiker vraagt om zijn uurloon, het aantal uur dat hij of zij gewerkt
+# heeft en dat daarna het salaris uitprint
+
+uurloon = float(input('Wat verdien je per uur:'))
+aantaluur= int(input('hoeveel uur heb je gewerkt:'))
+print(str(aantaluur)+' uur werken levert '+ str(uurloon*aantaluur)+' Euro op.')
+
+print('gittest')
