965ddea8029f879d689a597b1dc239e621c613f4
Antoon <antoon.vandenbosch@student.hu.nl>
2017-09-24 15:44:32 +0200

    directories aangepast

---
+++ Practice exercises/Practice Exercises 3/Practice Exercise 3_5.py
@@ -0,0 +1,5 @@
+# Practice Exercise 3_5: Schrijf een for-loop die over een lijst met getallen itereert, en alle even getallen print.
+
+for getal in range(10):
+    if getal%2 == 0:
+        print(getal)
---
+++ Practice exercises/Practice Exercises 3/Practice Exercise 3_6.py
@@ -0,0 +1,10 @@
+# Practice Exercise 3_6:
+# Schrijf een for-loop die langs alle letters van een string loopt en de letter uitprint, maar alleen als het
+# een klinker is ('aeiou'). Gebruik string s = "Guido van Rossum heeft programmeertaal
+# Python bedacht."
+
+s = 'Guido van Rossum heeft programmeertaalPython bedacht.'
+
+for klinker in s:
+    if klinker in 'aeoiu':
+        print(klinker, end='')
